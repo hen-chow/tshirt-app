a42d04b1502af63479c2f7c298ed5b4e3f7398b8
Henrietta Chow <henchow@gmail.com>
2017-04-10 22:10:48 +1000

    updated cart model, migrated combineItemsInCart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,18 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :line_items, dependent: :destroy
+
+  def add_product (product_id) # method to check whether list of items already incl the prod we're adding. if it does, it increases the qty, if it doesn't, it creates a new line_item
+    current_item = line_items.find_by_product_id(product_id) # use find_by_product_id method in ActiveRecord to search
+    if current_item
+      current_item.qty += 1
+    else
+      current_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
+
+  def total_price # add method to sum up items in cart
+    line_items.to_a.sum { |item| item.total_price }
+  end
 end
