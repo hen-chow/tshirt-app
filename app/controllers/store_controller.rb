e9ffe6fbf5ec6f212065d5f7a5190f140165243c
Henrietta Chow <henchow@gmail.com>
2017-04-10 14:34:03 +1000

    updated add image to tee and js selector

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,11 @@
 class StoreController < ApplicationController
 
+  SIZE_CONST = ["S", "M", "L"]
+
   def index
     @products = Product.all
     @designs = Design.order(:name)
+    @sizes = SIZE_CONST
   end
 
 end
