1cd70cb462069b7523f4d224a1a5178fcde52513
Henrietta Chow <henchow@gmail.com>
2017-04-11 12:55:49 +1000

    shopping cart nav bar amended

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -12,15 +12,15 @@ class LineItemsController < ApplicationController
     # @line_item = @cart.add_product(product.id, design.id, params[:qty], @cart.id) # telling it to call the add_product method in the cart model
     line_item = LineItem.create(cart_id: @cart.id, product_id: product.id, design_id: design.id, qty: params[:qty], top: params[:top], left: params[:left], width_ratio: params[:width_ratio], height_ratio: params[:height_ratio])
 
-    respond_to do |format|
-      if line_item.save
-        format.html { redirect_to cart_path(id: @cart.id) } # notice: 'Line item was successfully created.'
-        format.json { render :show, status: :created, location: line_item }
-      else
-        format.html { render :new }
-        format.json { render json: line_item.errors, status: :unprocessable_entity }
-      end
-    end
+    # respond_to do |format|
+    #   if line_item.save
+    #     format.html { redirect_to cart_path(id: @cart.id) } # notice: 'Line item was successfully created.'
+    #     format.json { render :show, status: :created, location: line_item }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: line_item.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   def update
