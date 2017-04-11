4e73ebee84e23b2b54bea34c384c12c26e90ec67
Henrietta Chow <henchow@gmail.com>
2017-04-11 12:15:02 +1000

    updated carts edit destroy function and cart show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :orders
   resources :carts
-  resources :line_items
+  resources :line_items, only: [:new, :create, :update, :destroy]
   resources :users
   resources :sessions
 
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get "signup", to: "users#new", as: "signup"
 
   get "signin", to: "sessions#new", as: "signin"
- post "signin", to: "sessions#create"
- delete "signout", to: "sessions#destroy", as: "signout"
+  post "signin", to: "sessions#create"
+  delete "signout", to: "sessions#destroy", as: "signout"
 
 end
