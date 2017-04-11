2522386ea240bef4b9f9333c0339b4305857ddd8
Jin Liaskos <jinliaskos@gmail.com>
2017-04-11 12:40:12 +1000

    Added cloudinary js image upload for designs

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -1,3 +1,21 @@
 class DesignsController < ApplicationController
+  before_action :authenticate_user, only: [:create]
+
+  def create
+    design = Design.new(clean_params.merge(user_id: session[:user_id], predesign: false))
+
+    if design.save
+      render json: design
+    else
+binding.pry
+      render json: {}, status: :bad_request
+    end
+  end
+
+  private
+
+  def clean_params
+    params.require(:design).permit(:name, :length, :width, :img_src)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # The root of site routed with store/index
+  root "store#index"
+
   resources :orders
   resources :carts
   resources :line_items
   resources :users
   resources :sessions
+  resources :designs, only: [:create]
 
-  get "store/index"
   resources :products, only: [:new]
-  # The root of site routed with store/index
-  root "store#index", as: "store"
 
   get "signup", to: "users#new", as: "signup"
 
   get "signin", to: "sessions#new", as: "signin"
- post "signin", to: "sessions#create"
- delete "signout", to: "sessions#destroy", as: "signout"
+  post "signin", to: "sessions#create"
+  delete "signout", to: "sessions#destroy", as: "signout"
 
 end
