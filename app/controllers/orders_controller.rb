6237697875b7c9d191c0c322e0fe16029ad0c4fd
Henrietta Chow <henchow@gmail.com>
2017-04-11 12:41:43 +1000

    orders controller with minor changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
 
   def new
+    @cart = current_cart
+    if @cart.line_items.empty?
+      redirect_to store_url, notice: "Your shopping cart is empty"
+    end
     @order = Order.new
   end
-  
+
 end
