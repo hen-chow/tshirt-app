7066816b303651def05a4d539daddf82ffcdf46c
Hen-Chow <henchow@gmail.com>
2017-04-12 13:13:02 +1000

    updated index page

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,13 @@ class Product < ApplicationRecord
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
+  validates :colour, :size, :img_src, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :img_src, allow_blank: true, format: {
+    with: %r{.(eps|gif|jpg|png)\Z}i,
+    message: 'must be an eps, gif, jpg or png image'
+  }
+
   SIZES = ["S", "M", "L"]
 
 
