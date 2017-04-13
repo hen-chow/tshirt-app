d61288a0245cb133c2428243e68f5105045d5e2c
Hen-Chow <henchow@gmail.com>
2017-04-13 16:59:10 +1000

    last update on app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-  def total_number_of_items_in_cart
-    @cart = current_cart
-    @total_qty =  @cart.line_items.to_a.sum { |item| item.qty}
-    @total_qty
-  end
+  # def total_number_of_items_in_cart
+  #   @cart = current_cart
+  #   @total_qty =  @cart.line_items.to_a.sum { |item| item.qty}
+  #   @total_qty
+  # end
 end
