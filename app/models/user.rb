605ded449c82edc2e71bb96539ac34d670a423a4
Hen-Chow <henchow@gmail.com>
2017-05-13 12:21:14 +1000

    tshirt resizable minor fix user email validation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :orders
   has_many :designs
   has_one :cart
+  validates_uniqueness_of :email, :case_sensitive => false, :with => /.+@.+\..+/i
   has_secure_password
 
 end
